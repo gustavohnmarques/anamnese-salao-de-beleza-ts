1677adcafd26109c468d2ffad075c50e6125dafa
Gustavo Marques <51138752+gustavohnmarques@users.noreply.github.com>
2024-05-05 20:59:35 -0300

    Refactor: Conversão do componente CorCabeloNaturalItem para styled components

--- src/components/Configs/CorCabeloNatural/CorCabeloNaturalItem.tsx
+++ src/components/Configs/CorCabeloNatural/CorCabeloNaturalItem.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Text, View, TouchableOpacity } from "react-native";
 import Icon from 'react-native-vector-icons/FontAwesome';
 import { StyleSheet, TextStyle, ViewStyle, Alert } from "react-native";
+import * as S from './styles';
 
 export type CorCabeloNatualItemProps = {
     id: Number,
@@ -15,7 +16,7 @@ export function CorCabeloNaturalItem(props: CorCabeloNatualItemProps): React.JSX
     const confirmarExclusao = () => {
         Alert.alert('Atenção!', `Deseja excluir ${props.descricao}?`, [
             {
-                text: 'Cancelar',                
+                text: 'Cancelar',
                 style: 'cancel',
             },
             { text: 'Sim, excluir', onPress: () => props.handleDelete(props.id) },
@@ -23,57 +24,20 @@ export function CorCabeloNaturalItem(props: CorCabeloNatualItemProps): React.JSX
     }
 
     return (
-        <View style={styles.item}>
-            <View style={styles.descricao}>
-                <Text style={styles.textoDescricao}>{props.descricao}</Text>
-            </View>
-            <View style={styles.acoes}>
-                <TouchableOpacity onPress={() => props.handleEdit(props.id)} style={styles.itemAcao}>
-                    <View>
-                        <Icon name="edit" size={26} color="#9d7cd9" />
-                    </View>
-                </TouchableOpacity>
+        <S.Item>
+            <S.Descricao >
+                <S.TextoDescricao>{props.descricao}</S.TextoDescricao>
+            </S.Descricao>
+            <S.Acoes>
+                <S.ItemAcao onPress={() => props.handleEdit(props.id)}>
+                    <Icon name="edit" size={26} color="#9d7cd9" />
+                </S.ItemAcao>
 
-                <TouchableOpacity onPress={confirmarExclusao} style={styles.itemAcao}>
-                    <View >
-                        <Icon name="trash-o" size={26} color="#ff5f56" />
-                    </View>
-                </TouchableOpacity>
-            </View>
-        </View>
+                <S.ItemAcao onPress={confirmarExclusao}>
+                    <Icon name="trash-o" size={26} color="#ff5f56" />
+                </S.ItemAcao>
+            </S.Acoes   >
+        </S.Item>
     )
 }
 
-const styles = StyleSheet.create({
-    item: {
-        height: 60,
-        width: '100%',
-        borderRadius: 5,
-        backgroundColor: '#fff',
-        flexDirection: 'row'
-    } as ViewStyle,
-
-    descricao: {
-        height: '100%',
-        width: '80%',
-        justifyContent: 'center',
-        paddingLeft: 15
-    } as ViewStyle,
-
-    acoes: {
-        height: '100%',
-        width: '20%',
-        flexDirection: 'row'
-    } as ViewStyle,
-
-    itemAcao: {
-        justifyContent: 'center',
-        alignItems: 'center',
-        flex: 1,
-    } as ViewStyle,
-
-    textoDescricao: {
-        color: '#747474',
-        fontSize: 18
-    } as TextStyle,
-});
---
+++ src/components/Configs/CorCabeloNatural/styles.ts
@@ -0,0 +1,43 @@
+import styled, { css } from "styled-components/native";
+
+export const Item = styled.View`    
+    ${({ theme }) => css`
+        height: 60px;
+        width: 100%;
+        border-radius: 5px;
+        background-color: #fff;
+        flex-direction: row;
+    `}
+`
+
+export const Descricao = styled.View`    
+    ${({ theme }) => css`
+        height: 100%;
+        width: 80%;
+        justify-content: center;
+        padding-left: 15px
+    `}
+`
+
+export const Acoes = styled.View`    
+    ${({ theme }) => css`
+        height: 100%;
+        width: 20%;
+        flex-direction: row;
+    `}
+`
+
+export const ItemAcao = styled.TouchableOpacity`    
+    ${({ theme }) => css`
+        justify-content: center;
+        align-items: center;
+        flex: 1;
+    `}
+`
+
+export const TextoDescricao = styled.Text`    
+    ${({ theme }) => css`
+        color: #747474;
+        font-size: 18px;
+    `}
+`
