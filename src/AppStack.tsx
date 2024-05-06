0974627d68291431faf82e7d6b1d56a25cd672bb
Gustavo Marques <51138752+gustavohnmarques@users.noreply.github.com>
2024-05-05 22:51:23 -0300

    Refactor: Adionado sistema de temas no header do

--- src/AppStack.tsx
+++ src/AppStack.tsx
@@ -3,19 +3,32 @@ import { createDrawerNavigator } from "@react-navigation/drawer";
 import Home from "./pages/Home/Home";
 import Menu from "./components/Menu/Menu";
 import CorCabeloNatural from "./pages/Configs/CorCabeloNatural/CorCabeloNatural";
+import { useTheme } from "./contexts/theme";
 const Drawer = createDrawerNavigator();
 
 const AppStack = () => {
+
+    const {getTheme} = useTheme();
+
+    const headerStyle = {
+        headerStyle: {
+            backgroundColor: getTheme().colors.background100,                        
+        },
+        headerTintColor:  getTheme().colors.textColor,
+        
+    }
+
     return (
         <Drawer.Navigator drawerContent={(props) => <Menu {...props} />}>
             <Drawer.Screen
                 name="Home"
                 component={Home}
-                options={{ headerShown: false }}
+                options={headerStyle}
             />
             <Drawer.Screen
                 name="CorCabelo"
                 component={CorCabeloNatural}
+                options={headerStyle}
             />
         </Drawer.Navigator>
     );
